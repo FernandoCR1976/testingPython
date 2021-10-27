19a444d22bb2b650d87ffb435c5cd286a809d69a
FernandoCR1976 <fernandocorralesm76@gmail.com>
2021-10-27 22:57:28 +0000

    firstupload

---
+++ testingsums.py
@@ -0,0 +1,21 @@
+l1=[3,7,8,9]
+cont=0
+suma=0
+while cont<len(l1):
+    suma=l1[cont]+suma
+    cont+=1
+print(suma)
+##############################
+def sumalista(listaNumeros):
+    laSuma = 0
+    for i in listaNumeros:
+        laSuma = laSuma + i
+    return laSuma
+###############################
+def sumlista(listaNum):
+    cont=0
+    suma=0
+    while cont<len(l1):
+        suma=l1[cont]+suma
+        cont+=1
+    return sumlista
